5ce2e39cd249f72b8ab3fc2ce88b4b20e2c0bb15
nicolas zulaica <nzulaica@fi.uba.ar>
2021-10-31 12:19:08 -0300

    cleanup boot_map region

--- kern/pmap.c
+++ kern/pmap.c
@@ -419,51 +419,59 @@ pgdir_walk(pde_t *pgdir, const void *va, int create)
 static void
 boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm)
 {
-#ifndef TP1_PSE
-	pte_t *pte;
-	size_t offset = PGSIZE;
-	while (size > 0) {
-		pte = pgdir_walk(pgdir, (void *) va, 1);  // Get table entry
-		if (pte == NULL) {
-			panic("pgdir_walk failed");
-		}
-		*pte = pa | perm |
-		       PTE_P;  // Set entry to physical address and permissions
-		va += offset;  // Increment virtual address
-		pa += offset;  // Increment physical address
-		if( size < offset ) return; // Return if size is less than offset. Checked to prevent overflow if not aligned.
-		size -= offset;  // Decrement remaining size
-	}
-#else
-	pte_t *pte;
 	pde_t *pde;
+	pte_t *pte;
 	size_t offset = PGSIZE;
-	size_t MiB = PGSIZE*NPDENTRIES;
 	while (size > 0) {
-
-		if(
-			size >= MiB &&
-			( pa & ( MiB - 1) ) == 0
-		){ // LARGE PAGE
+#ifdef TP1_PSE
+		if (size >= PTSIZE && (pa & (PTSIZE - 1)) == 0) {  // LARGE PAGE
 			pde = pgdir + PDX(va);
 			*pde = pa | perm | PTE_P | PTE_PS;
-			offset = MiB;
-		} else { // SMALL PAGE
+			offset = PTSIZE;
+		} else  // SMALL PAGE
+#endif
+		{
 			pte = pgdir_walk(pgdir, (void *) va, 1);  // Get table entry
 			if (pte == NULL) {
 				panic("pgdir_walk failed");
 			}
 			*pte = pa | perm |
-				PTE_P;  // Set entry to physical address and permissions
-			offset = PGSIZE;
+			       PTE_P;  // Set entry to physical address and permissions
 		}
-
 		va += offset;  // Increment virtual address
 		pa += offset;  // Increment physical address
-		if( size < offset ) return; // Return if size is less than offset. Checked to prevent overflow if not aligned.
+		if (size < offset)
+			return;  // Return if size is less than offset. Checked to prevent overflow if not aligned.
 		size -= offset;  // Decrement remaining size
 	}
-#endif
+	// pte_t *pte;
+	// pde_t *pde;
+	// size_t offset = PGSIZE;
+	// size_t MiB = PGSIZE*NPDENTRIES;
+	// while (size > 0) {
+
+	// 	if(
+	// 		size >= MiB &&
+	// 		( pa & ( MiB - 1) ) == 0
+	// 	){ // LARGE PAGE
+	// 		pde = pgdir + PDX(va);
+	// 		*pde = pa | perm | PTE_P | PTE_PS;
+	// 		offset = MiB;
+	// 	} else { // SMALL PAGE
+	// 		pte = pgdir_walk(pgdir, (void *) va, 1);  // Get table
+	// entry 		if (pte == NULL) { 			panic("pgdir_walk failed");
+	// 		}
+	// 		*pte = pa | perm |
+	// 			PTE_P;  // Set entry to physical address and
+	// permissions 		offset = PGSIZE;
+	// 	}
+
+	// 	va += offset;  // Increment virtual address
+	// 	pa += offset;  // Increment physical address
+	// 	if( size < offset ) return; // Return if size is less than
+	// offset. Checked to prevent overflow if not aligned. 	size -= offset;
+	// // Decrement remaining size
+	// }
 }
 
 //
