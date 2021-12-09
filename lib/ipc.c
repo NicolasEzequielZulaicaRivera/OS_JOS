db06911708e368a94743e6d240cc48cdd1bf4b62
nicozu <nzulaica@fi.uba.ar>
2021-12-08 21:58:41 -0300

    ipc_recv

--- lib/ipc.c
+++ lib/ipc.c
@@ -23,8 +23,25 @@ int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
 	// LAB 4: Your code here.
-	panic("ipc_recv not implemented");
-	return 0;
+	void *dstva = pg;
+	if (pg == NULL)
+		dstva = (void *) UTOP;
+
+	int r = sys_ipc_recv(dstva);
+	if (r < 0) {
+		if (from_env_store != NULL)
+			*from_env_store = 0;
+		if (perm_store != NULL)
+			*perm_store = 0;
+		return r;
+	}
+
+	if (from_env_store != NULL)
+		*from_env_store = thisenv->env_ipc_from;
+	if (perm_store != NULL)
+		*perm_store = thisenv->env_ipc_perm;
+
+	return thisenv->env_ipc_value;
 }
 
 // Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
