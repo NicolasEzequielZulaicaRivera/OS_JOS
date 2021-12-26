458c51d94b71c5f33b2b6501774b22c469c820f8
nicozu <nzulaica@fi.uba.ar>
2021-12-26 20:45:47 -0300

    check maxsize

--- lib/file.c
+++ lib/file.c
@@ -142,10 +142,12 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	// bytes than requested.
 	// LAB 5: Your code here
 	int r;
+	size_t max_size = PGSIZE - (sizeof(int) + sizeof(size_t)) -1;
+	size_t write_size =  MIN(n,max_size);
 
 	fsipcbuf.write.req_fileid = fd->fd_file.id;
-	fsipcbuf.write.req_n = n;  // TODO: check max size. Fsreq_write
-	memmove(fsipcbuf.write.req_buf, buf, n);
+	fsipcbuf.write.req_n = write_size;
+	memmove(fsipcbuf.write.req_buf, buf, write_size);
 
 	if ((r = fsipc(FSREQ_WRITE, NULL)) < 0)
 		return r;
